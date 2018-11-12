5f6f265fa0807aec29051e37ac322b88154fc046
Albdarm <albdarm@dewv.edu>
2018-11-12 17:59:54 +0000

    Updates via sync script

--- puzzledPrincess.js
+++ puzzledPrincess.js
@@ -6,9 +6,9 @@ class Marker extends Sprite {
         super();
         this.board = board;
         this.name = name;
-        this.setImage = image;
-        this.x = 150;
-        this.y = 275;
+        this.setImage(image);
+        this.x = this.startX = 150;
+        this.y = this.startY = 275;
     }
 }
 class PrincessMarker extends Marker {
@@ -21,11 +21,23 @@ class PrincessMarker extends Marker {
     }
     handleMouseLeftButtonUp() {
         this.dragging = false;
+        let row = Math.floor((this.y - this.board.y) / 150);
+        //window.alert(row);
+        if (col < 0 || col > 2) {
+            this.y = this.startY;
+            return;
+        }
+            let col = Math.floor((this.x - this.board.x) / 150);
+        //window.alert(col);
+        if (row < 0 || row > 2) {
+            this.x = this.startX;
+            return;
+        }
     }
     handleGameLoop() {
         if (this.dragging) {
             this.x = game.getMouseX() - this.width / 2;
-            this.y = game.getMouseX() - this.height / 2;
+            this.y = game.getMouseY() - this.height / 2;
         }
     }
 }
